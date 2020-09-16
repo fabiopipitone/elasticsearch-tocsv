4be4203653528467b5f44a07e3bc148634dbdbc0
Fabio Pipitone <fabio.pipitone93@gmail.com>
2020-09-16 12:34:36 +0200

    Build ES_QUERY from query string and correct range and arrange single-multithread division

--- esToCsv.py
+++ esToCsv.py
@@ -7,10 +7,6 @@ from datetime import datetime, timedelta
 # Disable warning about not using an ssl certificate
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
-############### PRINT SCRIPT RUN ##############
-print('################ LAUNCHING THE WEB SCRIPT -- DATETIME {} ################\n'.format(datetime.now().strftime('%d/%m/%Y_%H:%M:%S')))
-################## END BLOCK ##################
-
 ############ UTILITY FUNCTIONS ############
 def obj_to_list(object, fields):
   list_of_values = []
@@ -33,7 +29,7 @@ ap.add_argument("-e", "--export_path", required=False, help="path where to store
 ap.add_argument("-sd", "--starting_date", required=False, help="query starting date")
 ap.add_argument("-ed", "--ending_date", required=False, help="query ending date")
 ap.add_argument("-t", "--time_field", required=False, help="time field to query on. If not set and --starting_date or --ending_date are set and exception will be raised")
-ap.add_argument("-q", "--query_string", required=False, help="Elasticsearch query string. Put it between quotes and escape internal quotes characters (e.g. \"one_field: foo AND another_field.keyword: \\\"bar\\\"\"")
+ap.add_argument("-q", "--query_string", required=False, help="Elasticsearch query string. Put it between quotes and escape internal quotes characters (e.g. \"one_field: foo AND another_field.keyword: \\\"bar\\\"\"", default="*")
 ap.add_argument("-p", "--port", required=False, help="Elasticsearch port. If not set, the default port 9200 will be used", default=9200, type=int)
 ap.add_argument("-u", "--user", required=False, help="Elasticsearch user", default='')
 ap.add_argument("-pw", "--password", required=False, help="Elasticsearch password in clear. If set, the --secret_password will be ignored")
@@ -49,13 +45,15 @@ args = vars(ap.parse_args())
 ########## END SCRIPT ARGUMENTS AND OPTIONS ##########
 
 ############ CHECK ARGUMENTS CONFLICTS ############
-MULTIPROCESSING_ENABLED = args['enable_multiprocessing']
 if (args['starting_date'] != None or args['ending_date'] != None) and args['time_field'] == None:
   sys.exit("\nIf you set either a starting_date or an ending_date you have to set a --time_field to sort on, too.")
 
+MULTIPROCESSING_ENABLED = args['enable_multiprocessing']
 if MULTIPROCESSING_ENABLED and args['time_field'] == None:
   sys.exit("\nYou have to set a --time_field in order to use multiprocessing.")
 ########## END CHECK ARGUMENTS CONFLICTS ##########
+
+print('################ LAUNCHING THE WEB SCRIPT -- DATETIME {} ################\n'.format(datetime.now().strftime('%d/%m/%Y_%H:%M:%S')))
 
 ############ GLOBAL VARIABLES ############
 TOTAL_THREADS = min(multiprocessing.cpu_count(), safe_toint_cast(args['threads'])) if args['threads'] != None else multiprocessing.cpu_count()
@@ -67,26 +65,24 @@ if os.path.exists(EXPORT_PATH):
   overwrite_file = ''
   while overwrite_file not in ['y', 'n']:
     overwrite_file = input("\nThere is already a csv file at the given path ({}). Do you want to overwrite it? (y/n)".format(EXPORT_PATH)).lower().strip()
-  overwrite_file = True if overwrite_file == 'y' else False
+  if overwrite_file == 'n':
+    sys.exit("\nExiting script not to overwrite the file. No query has been run.")
 
-if not overwrite_file:
-  sys.exit("\nExiting script not to overwrite the file. No query has been run.")
 
-if args['es_query'] != None: # TODO check query is legit
-  ES_QUERY = args['es_query']
-elif args['starting_date'] != None or args['ending_date'] != None:
+# Build ES query
+QUERY_STRING = args['query_string'] #TODO chech how to properly escape internal quotes
+if args['time_field'] != None:
   TIME_FIELD = args['time_field']
-  FROM_DATE = args['starting_date']
-  TO_DATE = args['ending_date']
-  ES_QUERY = '{"query":{"bool":{"must":[{"range":{"' + TIME_FIELD + '":{"gt":"' + FROM_DATE + '","lte":"' + TO_DATE + '"}}}]}}}'
+  FROM_DATE = args['starting_date'] if args['starting_date'] != None else "now-1000y"
+  TO_DATE = args['ending_date'] if args['ending_date'] != None else "now+1000y"
+  RANGE_QUERY = ",{\"range\":{\"" + TIME_FIELD + "\":{\"gte\":\"" + FROM_DATE + "\",\"lte\":\"" + TO_DATE + "\"}}}"
 else:
-  ES_QUERY = '{"query":{"match_all":{}}}'
+  RANGE_QUERY = ''
 
-TOTAL_HITS = 0
+ES_QUERY = '{"query":{"bool":{"must":[{"query_string":{"query":"' + QUERY_STRING + '"}}' + RANGE_QUERY + ']}}}'
 ########## END GLOBAL VARIABLES ##########
 
 ############ ES CONNECTION PARAMS ############
-
 FROM_USERNAME = args['user']
 FROM_PW = args['password'] if args['password'] != None else os.environ[args['secret_password']] if args['secret_password'] != None and args['secret_password'] in os.environ else ''
 FROM_SSL = args['ssl']
@@ -111,45 +107,55 @@ FROM_BATCH_SIZE = args['batch_size']
 
 
 ################ RETRIEVE DATA ################
-# Connect to ES server
-try:
-  my_data = FROM_ES.search(
-    index = FROM_INDEX,
-    _source = FROM_SOURCE,
-    scroll = FROM_SCROLL_TIMEOUT,
-    size = FROM_BATCH_SIZE,
-    body = FROM_BODY
-  )
-except Exception as e:
-  print("\nSomething went wrong when fetching the data from Elasticsearch. Please check your connection parameters. Here's the raised exception: \n\n{}".format(e))
 
-sid = my_data['_scroll_id']
-scroll_size = len(my_data['hits']['hits'])
-page_counter = 1
+if MULTIPROCESSING_ENABLED:
+  # Do the multithread code
+  print('MULTITHREAD PROCESSING\n')
+else: 
+  print('SINGLE THREAD PROCESSING\n')
+  TOTAL_HITS = 0
 
-# Process current batch of hits before starting to scroll
-for hit in my_data['hits']['hits']:
-  TOTAL_HITS += 1
-  FETCHED_DATA.append(obj_to_list(hit['_source'], FIELDS_OF_INTEREST))
+  # Connect to ES server
+  try:
+    my_data = FROM_ES.search(
+      index = FROM_INDEX,
+      _source = FROM_SOURCE,
+      scroll = FROM_SCROLL_TIMEOUT,
+      size = FROM_BATCH_SIZE,
+      body = FROM_BODY
+    )
+  except Exception as e:
+    print("\nSomething went wrong when fetching the data from Elasticsearch. Please check your connection parameters. Here's the raised exception: \n\n{}".format(e))
+  
+  # Save parameters for scrolling
+  sid = my_data['_scroll_id']
+  scroll_size = len(my_data['hits']['hits'])
+  page_counter = 1
 
-# Scroll and add hits to the FETCHED_DATA list
-while scroll_size > 0:
-  print('\rScrolling...(page {})'.format(page_counter), end = '')
-  my_data = FROM_ES.scroll(scroll_id=sid, scroll=FROM_SCROLL_TIMEOUT)
-  # Process current batch of hits
+  # Process current batch of hits before starting to scroll
   for hit in my_data['hits']['hits']:
     TOTAL_HITS += 1
     FETCHED_DATA.append(obj_to_list(hit['_source'], FIELDS_OF_INTEREST))
-  # Update the scroll ID
-  sid = my_data['_scroll_id']
-  # Get the number of results that returned in the last scroll
-  scroll_size = len(my_data['hits']['hits'])
-  page_counter += 1
 
-print("\n\n" + str(TOTAL_HITS) + ' documents have been fetched and processed. FETCHED_DATA list has been created.')
+  # Scroll and add hits to the FETCHED_DATA list
+  while scroll_size > 0:
+    print('\rScrolling...(page {})'.format(page_counter), end = '')
+    my_data = FROM_ES.scroll(scroll_id=sid, scroll=FROM_SCROLL_TIMEOUT)
+    # Process current batch of hits
+    for hit in my_data['hits']['hits']:
+      TOTAL_HITS += 1
+      FETCHED_DATA.append(obj_to_list(hit['_source'], FIELDS_OF_INTEREST))
+    # Update the scroll ID
+    sid = my_data['_scroll_id']
+    # Get the number of results that returned in the last scroll
+    scroll_size = len(my_data['hits']['hits'])
+    page_counter += 1
+
+  print("\n\n" + str(TOTAL_HITS) + ' documents have been fetched and processed. FETCHED_DATA list has been created.')
+
 
 # Writing the csv file
-print('\nWriting the csv file from the DataFrame...')
+print('\nWriting the csv file...')
 with open(EXPORT_PATH, 'w', newline='') as csvfile:
   writer = csv.writer(csvfile)
   writer.writerows(FETCHED_DATA)
