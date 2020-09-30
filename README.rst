c0184fddaedc0ca93d45e06d0eb8e4f7f2596eb1
Fabio Pipitone <fabio.pipitone93@gmail.com>
2020-09-30 16:16:41 +0200

    Add basic README.srt

--- README.rst
+++ README.rst
@@ -1,1 +1,23 @@
- 
+Python Elasticsearch-to-CSV Export Tool
+=======================================
+
+Simple Python tool to easily extract a massive amount of Elasticsearch documents into a csv file, exploiting multiprocessing features and leveraging the underneath elasticsearch-py package.
+
+Requirements
+------------
+
+In order to install and use the elasticsearch-tocsv tool, you need to install the following packages first:
+
+  * `elasticsearch-py <https://pypi.org/project/elasticsearch>`_
+  * `requests <https://pypi.org/project/requests/>`_
+  * `pandas <https://pypi.org/project/pandas/>`_
+  * `pytz <https://pypi.org/project/pytz/>`_
+  * `tqdm <https://pypi.org/project/tqdm/>`_ (unless you want to disable the loading progress bars)
+
+
+Installation
+------------
+
+Install the ``elasticsearch`` package with `pip <https://pypi.org/project/elasticsearchtocsv>`_:
+
+    $ python -m pip install elasticsearchtocsv
--- setup.py
+++ setup.py
@@ -3,11 +3,11 @@ import pathlib
 
 here = pathlib.Path(__file__).parent.resolve()
 
-long_description = (here / 'README.md').read_text(encoding='utf-8')
+long_description = (here / 'README.rst').read_text(encoding='utf-8')
 
 setup(
-    name='elasticsearch-tocsv',
-    version='0.9.0',
+    name='elasticsearch_tocsv',
+    version='0.0.1',
     description='Simple python tool to extract massive amounts of documents from Elasticsearch into a csv file exploiting multiprocessing and leveraging the underneath elasticsearch-py package',
     long_description=long_description,
     long_description_content_type='text/x-rst',
@@ -15,13 +15,13 @@ setup(
     author='Fabio Pipitone',
     author_email='fabio.pipitone93@gmail.com',
     keywords='elasticsearch, export, csv, report, kibana, elasticsearch-py',
-    package_dir={'': 'elasticsearch-tocsv'},
-    packages=find_packages(where='elasticsearch-tocsv'),
+    package_dir={'': 'elasticsearch_tocsv'},
+    packages=find_packages(where='elasticsearch_tocsv'),
     python_requires='>=3.5, <4',
     install_requires=['requests>==2.22.0', 'elasticsearch>=7.9.1', 'tqdm>=4.49.0', 'pandas>=1.1.2', 'pytz>=2020.1'],
     entry_points={ 
         'console_scripts': [
-            'elasticsearch-tocsv=elasticsearch-tocsv.elasticsearch-tocsv:main',
+            'elasticsearch_tocsv=elasticsearch_tocsv.elasticsearch_tocsv:main',
         ],
     },
 )
