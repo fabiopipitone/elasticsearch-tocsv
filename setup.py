4aae9521f8fe93e760dcfb62e0ee1f5631926475
Fabio Pipitone <fabio.pipitone93@gmail.com>
2020-10-28 21:17:39 +0100

    Update version given the bug fixing

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.rst", "r") as fh:
 
 setup(
   name='elasticsearch-tocsv',
-  version='0.1.3',
+  version='0.1.4',
   author='Fabio Pipitone',
   author_email='fabio.pipitone93@gmail.com',
   url='https://github.com/fabiopipitone/elasticsearch-tocsv',
